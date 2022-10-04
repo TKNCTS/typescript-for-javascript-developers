6f01f757980eb986189ab616ecf743da03165dac
Konishi <konishi.tomoki@jp.fujitsu.com>
2022-10-04 14:31:53 +0900

    making file

---
+++ src/number-string.ts
@@ -0,0 +1,13 @@
+export {};
+
+//number型
+let year: number = 1976;
+//year = false;
+console.log(year);
+
+let age: number = 0x2b; //16進数
+console.log(age);
+
+//文字列型
+let name: string = "Ham";
+console.log(name);
---
+++ src/tuple.ts
@@ -0,0 +1,4 @@
+export {};
+
+//tuple
+let profile: [String, number] = ["Ham", 43]; //:以降で順番の制約を付けている
