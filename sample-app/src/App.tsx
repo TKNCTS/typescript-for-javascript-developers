b875f8994364d3b657aa8a857a5a0462b76ef9d3
Konishi <konishi.tomoki@jp.fujitsu.com>
2022-10-17 13:36:10 +0900

    Make sample-app file

--- sample-app/src/App.tsx
+++ sample-app/src/App.tsx
@@ -1,29 +1,20 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import CounterWidhReducer from "./CounterWidhReducer";
 
-function App(props: any) {
-  return (
-    console.log(props),
-    (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    )
-  );
+interface AppProps {
+  message?: string;
 }
+
+const App: React.FunctionComponent<AppProps> = ({ message }) => {
+  return (
+    <div>
+      <CounterWidhReducer />
+    </div>
+  );
+};
+
+App.defaultProps = {
+  message: "Hello,defaultProps!",
+};
 
 export default App;
